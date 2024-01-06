a7860a18875aa0df7380f043716ce43a4063eb8c
Felipe <felipevalerioderamos@gmail.com>
2024-01-06 14:45:00 -0300

    Defining the page configuration

---
+++ src/page/const.rs
@@ -0,0 +1,8 @@
+// The definition of the page file are defined here
+
+// size of the page file (bytes)
+pub const page_size: u32 = 4086;
+
+
+// size of the header of the page file (bytes)
+pub const header_size: u32 = 100;
