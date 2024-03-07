93327f3bcb00acdd78b73de5f974407680bcc495
Felipe <felipevalerioderamos@gmail.com>
2024-03-06 23:24:05 -0300

    writing data on disk

--- src/storage/storage.rs
+++ src/storage/storage.rs
@@ -1,8 +1,13 @@
+use std::fs::File;
+use std::io::Write;
 use std::collections::HashMap;
 
 pub fn put (data_map: &mut HashMap<String, String>, key: &String, value: &String) {
 	
 	data_map.insert(key.to_string(), value.to_string());
+
+	let mut file = File::create("db_file/database.txt").unwrap();
+	file.write_all(data_map).unwrap();
 }
 
 
