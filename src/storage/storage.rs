416f149495fc2e0a83abb46b1cd5522b74cace0b
Felipe <felipevalerioderamos@gmail.com>
2024-03-20 00:25:43 -0300

    persisting data on disk

--- src/storage/storage.rs
+++ src/storage/storage.rs
@@ -1,9 +1,11 @@
 use std::collections::HashMap;
+use bincode::{Decode, Encode};
 
 pub fn put(data_map: &mut HashMap<String, String>, key: &String, value: &String) {
 	
 	data_map.insert(key.to_string(), value.to_string());
 
+	let encoded: Vec<u8> = bincode::encode_to_vec(&data_map).unwrap();
 }
 
 
