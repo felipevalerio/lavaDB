22652127e2df887a7790c6e5cd7f772b0ed9fce5
Felipe <felipevalerioderamos@gmail.com>
2024-03-20 10:30:17 -0300

    persisting data to disk

--- src/storage/storage.rs
+++ src/storage/storage.rs
@@ -1,11 +1,23 @@
+use std::fs;
+use std::io::Write;
+use std::io::Result;
 use std::collections::HashMap;
-use bincode::{Decode, Encode};
+use serde::{Serialize, Deserialize};
 
 pub fn put(data_map: &mut HashMap<String, String>, key: &String, value: &String) {
 	
 	data_map.insert(key.to_string(), value.to_string());
+	save_to_disk(data_map);
+}
 
-	let encoded: Vec<u8> = bincode::encode_to_vec(&data_map).unwrap();
+
+fn save_to_disk(data_map: &mut HashMap<String, String>) -> Result<T> {
+	let encoded: Vec<u8> = bincode::serialize(&data_map).unwrap();
+	let mut file = fs::OpenOptions::new()
+								.write(true)
+								.open("src/db.bin");
+	
+	file.write_all(&encoded);
 }
 
 
